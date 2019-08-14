144c339bf595ab5ec45116466998671e499e5914
Muhammad Rizki A.R.M <rizki.idea@gmail.com>
2019-08-14 23:46:07 +0800

    Add allowed hosts

--- aiVLE/settings.py
+++ aiVLE/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cs4246-i.comp.nus.edu.sg', '127.0.0.1']
 
 
 # Application definition
